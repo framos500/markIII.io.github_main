4c839149e26912289828175ff18f5f855ece2214
framos500 <142738685+framos500@users.noreply.github.com>
2024-01-01 20:57:32 -0300

    Update and rename repo.sh to diretório.sh

---
+++ diretório.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Criar o diretório
+mkdir -p /github/workspace/docs
+
+# Instalar Bundler na versão 2.4.22
+gem install bundler -v 2.4.22
+
+# Verificar se a instalação do Bundler foi bem-sucedida
+if [ $? -eq 0 ]; then
+    echo "Bundler instalado com sucesso!"
+else
+    echo "Erro ao instalar o Bundler."
+    exit 1  # Encerra o script com código de saída 1 em caso de erro
+fi
+
+# Exibir mensagem
+echo "Olá, Mundo!"
