a3de34e250a1831da5afc5bd3ec17eb776639f51
framos500 <142738685+framos500@users.noreply.github.com>
2024-01-02 00:17:08 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,6 +3,6 @@
 # Se necessário, adicione comandos adicionais aqui antes da construção do Jekyll
 
 # Construa o site Jekyll
-bundle exec jekyll build
+bundle exec jekyll build --source markIII.io.github_main/github/workflows/docs --destination /path/to/destination
 
 # Se necessário, adicione comandos adicionais aqui após a construção do Jekyll
