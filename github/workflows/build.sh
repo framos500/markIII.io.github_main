8773f1245015d9fcc9691253e01290bbbe1a6275
framos500 <142738685+framos500@users.noreply.github.com>
2024-01-01 23:53:45 -0300

    Create build.sh

---
+++ github/workflows/build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Se necessário, adicione comandos adicionais aqui antes da construção do Jekyll
+
+# Construa o site Jekyll
+bundle exec jekyll build
+
+# Se necessário, adicione comandos adicionais aqui após a construção do Jekyll
